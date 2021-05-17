231e225a8615f86815b7af79b0aea359009fa57c
Ninna Manzano <ninna_manzano@dlsu.edu.ph>
2021-05-17 18:59:25 +0800

    component declarations for Login and Register

--- app/src/main/java/com/mobdeve/yapr/iwiw/LoginActivity.java
+++ app/src/main/java/com/mobdeve/yapr/iwiw/LoginActivity.java
@@ -3,12 +3,26 @@ package com.mobdeve.yapr.iwiw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    // TAG declarations
+    public static final String LOGIN_ACTIVITY = "Login Activity";
+
+    // component declarations
+    private EditText etEmail;
+    private EditText etPass;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.etEmail = findViewById(R.id.login_etEmail);
+        this.etPass = findViewById(R.id.login_etPass);
+        this.btnLogin = findViewById(R.id.login_btnLogin);
+
     }
 }
